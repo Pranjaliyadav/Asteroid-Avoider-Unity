4238403b059ee3579c0de1ee15952d063be1d615
Pranjali Yadav <yadavpranjali1223@gmail.com>
2023-03-05 23:39:42 +0530

    setup

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main; //this access the main camera
+    }
+
+    private void Update()
+    {
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+            Debug.Log(touchPosition);
+
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+            Debug.Log(worldPosition);
+
+        }
+
+    }
+}

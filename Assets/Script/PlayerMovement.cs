c5784eae68a921da2275864a856f4d546dafcd78
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2023-03-07 23:12:45 +0530

    rotation

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float forceMagnitude;
     [SerializeField] private float maxVelocity;
+    [SerializeField] private float rotateSpeed;
 
     private Camera mainCamera;
     private Rigidbody rb;
@@ -21,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
     {
         ProcessInput();
         KeepPlayerOnScreen();
-        
+        RotateToFaceVelocity();
 
     }
     private void FixedUpdate()
@@ -38,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(movementDirection * forceMagnitude * Time.deltaTime, ForceMode.Force);
 
         rb.velocity =  Vector3.ClampMagnitude(rb.velocity, maxVelocity); //wont let rb velocity go more than maxVelocity.
-
+        //rigid body's velocity also tells the direction we are moving in. so can be used for rotations also 
     }
     private void ProcessInput()
     {
@@ -89,5 +90,15 @@ public class PlayerMovement : MonoBehaviour
         }
 
         transform.position = newPosition;
+    }
+
+    private void RotateToFaceVelocity()
+    {   
+        if(rb.velocity == Vector3.zero) { return; }
+
+        Quaternion targetRotation =  Quaternion.LookRotation(rb.velocity,Vector3.back);
+        
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        //when we are farther from our rotation location we rotate faster, when we are close we rotate slower
     }
 }

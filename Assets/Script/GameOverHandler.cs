136ec0951cc75d357cb287ad183daf0f9fc8003d
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2023-03-09 00:20:50 +0530

    game over

---
+++ Assets/Script/GameOverHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverDisplay;
+
+    [SerializeField] private AsteroidSpawner asteroidSpawner;
+    public void EndGame()
+    {
+        asteroidSpawner.enabled = false; //asteroid wont instantiate on screem
+
+        gameOverDisplay.gameObject.SetActive(true);
+    }
+   public void Playagain()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-
+    [SerializeField] private GameOverHandler gameOverHandler;
     public void Crash()
     {
+        gameOverHandler.EndGame();
         gameObject.SetActive(false);
     }
 }
